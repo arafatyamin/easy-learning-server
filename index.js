7df0dddc3f59706dbcb8b84e4222a438ddf636e5
arafatyamin <mdh631987@gmail.com>
2022-10-26 00:42:45 +0600

    create course server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+
+
+app.get('/', (req, res) =>{ 
+    res.send('courses api running')
+})
+
+
+app.listen(port, () => {
+    console.log(`course server running port ${port}`);
+})
